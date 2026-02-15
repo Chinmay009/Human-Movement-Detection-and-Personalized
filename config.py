02db229d2006941e41470bbbfa3ba9a63ab276d0
Chinmay Pawar <63091493+Chinmay009@users.noreply.github.com>
2026-02-15 18:10:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+OLLAMA_MODEL = "llama3"
+LLM_HOST = "localhost"
+
+SUMMARY_INTERVAL_MIN = 15
+MAX_RUNNING_DURATION_MIN = 20
+
+THRESHOLDS = {
+    "Walking": 120,
+    "Running": 20,
+    "Stepping": 100,
+    "Resting": 180
+}
+
+USER_PROFILE = {
+    "Age": 26,
+    "Gender": "male",
+    "Weight": 65,
+    "Location": "Mumbai",
+    "Goal": "Fat Cutting"
+}
